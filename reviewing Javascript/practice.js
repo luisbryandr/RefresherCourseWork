ff84684c555f6af7d1d78705d8132e139991d141
luisbryandr <luisbdominguezr@gmail.com>
2025-04-06 23:54:39 -0400

    just some notes on where i am with my journey

--- reviewing Javascript/practice.js
+++ reviewing Javascript/practice.js
@@ -1,1 +1,2 @@
 //starting  today with refresher on data types and Variables
+//finally recovered from being sick with the flu for 2 weeks so picking back up with JS -- reminder to still keep my CSS skills fresh byh worming on my site or other work
