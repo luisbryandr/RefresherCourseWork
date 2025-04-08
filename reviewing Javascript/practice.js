f3925b9bc7476ee0ce2bea602e6d3dd08c2106eb
luisbryandr <luisbdominguezr@gmail.com>
2025-04-07 23:54:19 -0400

    to document that im focusing on networking and researching tonight more than directly coding but still working on my development/ journey

--- reviewing Javascript/practice.js
+++ reviewing Javascript/practice.js
@@ -1,2 +1,3 @@
 //starting  today with refresher on data types and Variables
 //finally recovered from being sick with the flu for 2 weeks so picking back up with JS -- reminder to still keep my CSS skills fresh byh worming on my site or other work
+//doing more networking and researching tonight instead of coding -4/7/25
