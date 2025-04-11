18e5bf61a7cf8a57d628d735722ea74e6f263399
luisbryandr <luisbdominguezr@gmail.com>
2025-04-10 21:05:18 -0400

    just some more notes

--- reviewing Javascript/practice.js
+++ reviewing Javascript/practice.js
@@ -1,3 +1,4 @@
 //starting  today with refresher on data types and Variables
 //finally recovered from being sick with the flu for 2 weeks so picking back up with JS -- reminder to still keep my CSS skills fresh byh worming on my site or other work
 //doing more networking and researching tonight instead of coding -4/7/25
+// more just studying none coding but still doing my best to make progress on my journey
