766a6f6fd401458cdb27de28f2ddbc01e1ceee96
luisbryandr <luisbdominguezr@gmail.com>
2025-04-15 23:35:22 -0400

    update to progress

--- reviewing Javascript/practice.js
+++ reviewing Javascript/practice.js
@@ -2,3 +2,4 @@
 //finally recovered from being sick with the flu for 2 weeks so picking back up with JS -- reminder to still keep my CSS skills fresh byh worming on my site or other work
 //doing more networking and researching tonight instead of coding -4/7/25
 // more just studying none coding but still doing my best to make progress on my journey
+// trying not to let the past repeat itself and stop my month+ of consistent coding and learning. but as before family and money take priority and so its tough but looking to at least compromise with 30 to 60 mins a day
